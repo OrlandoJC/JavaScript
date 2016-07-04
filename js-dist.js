2fcd5443c413eac1fd21e597d7221c077d0929c6
orlando de jesus <cuximyamaorlandodejesus@gmail.com>
2016-07-04 01:27:04 -0500

    un cambio

---
+++ js-dist.js
@@ -0,0 +1,17 @@
+
+
+// (function(num1, num2){
+//    var total = num1 + num2;
+//    alert(total);
+
+// })(7, 5);
+
+
+var num1= 3, num2= 4;
+
+function suma(){
+  return num1 + num2;
+}
+
+alert(suma());
+//# sourceMappingURL=js-dist.js.map
---
+++ js.js
@@ -0,0 +1,16 @@
+
+
+// (function(num1, num2){
+//    var total = num1 + num2;
+//    alert(total);
+
+// })(7, 5);
+
+
+var num1= 3, num2= 4;
+
+function suma(){
+  return num1 + num2;
+}
+
+alert(suma());
